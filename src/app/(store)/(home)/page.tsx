efb278c2e03e06bc330ace5b0699ec398e3bc543
Caio Sella <sellacaio4@gmail.com>
2023-12-18 22:09:13 -0300

    implement grid with products layout

--- src/app/(store)/(home)/page.tsx
+++ src/app/(store)/(home)/page.tsx
@@ -1,5 +1,71 @@
-export default async function Home() {
-  await new Promise((resolve) => setTimeout(resolve, 3000))
+import Image from 'next/image'
+import Link from 'next/link'
 
-  return <h1>Caio</h1>
+export default async function Home() {
+  return (
+    <div className="grid max-h-[860px] grid-cols-9 grid-rows-6 gap-6">
+      <Link
+        href="/"
+        className="group relative col-span-6 row-span-6 rounded-lg bg-zinc-900 overflow-hidden"
+      >
+        <Image
+          className="group-hover:scale-105 transition-transform duration-500"
+          width={920}
+          height={920}
+          quality={100}
+          src="/moletom-never-stop-learning 1.png"
+          alt="image-moletn"
+        />
+
+        <div className="absolute bottom-20 right-28 h-12 flex items-center gap-2 max-w[200px] rounded-full border-2 border-zinc-500 bg-black p-1 pl-5">
+          <span className="text-sm truncate text-zinc-200">Moletom IA</span>
+          <span className="flex h-full items-center justify-center rounded-full bg-violet-500 px-4 font-semibold">
+            $129
+          </span>
+        </div>
+      </Link>
+
+      <Link
+        href="/"
+        className="group relative col-span-3 row-span-3 rounded-lg bg-zinc-900 overflow-hidden"
+      >
+        <Image
+          className="group-hover:scale-105 transition-transform duration-500"
+          width={920}
+          height={920}
+          quality={100}
+          src="/moletom-ia-p-devs.png"
+          alt="image-moletn"
+        />
+
+        <div className="absolute bottom-10 right-28 h-12 flex items-center gap-2 max-w[200px] rounded-full border-2 border-zinc-500 bg-black p-1 pl-5">
+          <span className="text-sm truncate text-zinc-200">Moletom IA</span>
+          <span className="flex h-full items-center justify-center rounded-full bg-violet-500 px-4 font-semibold">
+            $110
+          </span>
+        </div>
+      </Link>
+
+      <Link
+        href="/"
+        className="group relative col-span-3 row-span-3 rounded-lg bg-zinc-900 overflow-hidden"
+      >
+        <Image
+          className="group-hover:scale-105 transition-transform duration-500"
+          width={920}
+          height={920}
+          quality={100}
+          src="/camiseta-dowhile-2022.png"
+          alt="image-moletn"
+        />
+
+        <div className="absolute bottom-10 right-28 h-12 flex items-center gap-2 max-w[200px] rounded-full border-2 border-zinc-500 bg-black p-1 pl-5">
+          <span className="text-sm truncate text-zinc-200">Moletom IA</span>
+          <span className="flex h-full items-center justify-center rounded-full bg-violet-500 px-4 font-semibold">
+            $69
+          </span>
+        </div>
+      </Link>
+    </div>
+  )
 }
--- src/app/(store)/layout.tsx
+++ src/app/(store)/layout.tsx
@@ -6,7 +6,7 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <div className="mx-auto min-h-screen w-full max-w-[1680px] grid-rows-[min-content_max-content] gap-5 px-8 p-8">
+    <div className="mx-auto grid min-h-screen w-full max-w-[1600px] grid-rows-app gap-5 px-8 py-8">
       <Header />
       {children}
     </div>
